9b23db13800faa8fa43143234734013b568366db
yashhirudkar <akashsawlikar321@gmail.com>
2026-03-05 13:59:29 +0530

    we added some lines

--- demo.py
+++ demo.py
@@ -1,6 +1,22 @@
 print("hello good morning")
 print("hello")
 print("how are you")
+print("happy holi")
+print("hello good morning")
+print("hello")
+print("how are you")
+print("happy holi")
+
+print("hello good morning")
+print("hello")
+print("how are you")
+print("happy holi")
+
+print("hello good morning")
+print("hello")
+print("how are you")
+print("happy holi")
+
 
 
 
